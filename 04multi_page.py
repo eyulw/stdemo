bd8c3635606785f5526b35afb1c43669fde2a015
kimminha <98391462+eyulw@users.noreply.github.com>
2023-10-25 14:04:16 +0900

    Add files via upload
    upload main page

---
+++ 04multi_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+
+# 멀피 페이지용 제목
+st.set_page_config(page_title='Hello, Streamlit!',
+                   page_icon='😺')
+st.sidebar.header('Hello, Streamlit!')
+
+st.write('Welcome to Streamlit!!')
